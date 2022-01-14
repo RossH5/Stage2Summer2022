4046cd02a4f79807682e36710681b779beba69cf
Ross Henke <rosshenke91@gmail.com>
2022-01-14 13:04:58 -0600

    adding changes

--- module-12-rest-api/module-12-rest-api/Startup.cs
+++ module-12-rest-api/module-12-rest-api/Startup.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using module_12_rest_api.Daos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,21 +29,22 @@ namespace module_12_rest_api
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+
+            services.AddDbContext<ProductContext>(opt => opt.UseInMemoryDatabase("Products"));
+            services.AddApiVersioning(opt => opt.ReportApiVersions = true);
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "module_12_rest_api", Version = "v1" });
-            });
+                Title = "Products",
+                Description = "The ultimate e-commerce store for all your needs",
+                Version = "v1"
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "module_12_rest_api v1"));
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI(opt => opt.SwaggerEndpoint("/swagger/v1/swagger.json", "Products v1"));
 
             app.UseRouting();
 
